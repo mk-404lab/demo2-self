959c7d689290bd783deb4ee26fa053c83cdce324
mk-404lab <alsgur9984@naver.com>
2026-03-04 01:35:00 +0900

    feat: 8강 - 자바적 사고를 통해 답글->질문, 질문->답글 조회 방법 구현

--- src/main/java/com/back/Demo2SelfApplication.java
+++ src/main/java/com/back/Demo2SelfApplication.java
@@ -8,6 +8,16 @@ public class Demo2SelfApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo2SelfApplication.class, args);
+
+        // 답글로부터 질문을 찾아가는 방법
+        Answer a = new Answer();
+
+        Question question = a.getQuestion();
+        question.getContent();
+
+        // 특정 질문에 달린 모든 답글
+        Question q1 = questionRepository.findById(1);
+        q1.getAnswerList();
     }
 
 }
