bf881d74414367c9744a569e5b795f59491c512d
mk-404lab <alsgur9984@naver.com>
2026-03-03 20:09:16 +0900

    feat: 1~3강 - 초기 셋팅, MainController 생성

---
+++ src/test/java/com/back/Demo2SelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2SelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
