f15ad35a47061d81394fe03a903ea0c57c5236a9
mk-404lab <alsgur9984@naver.com>
2026-03-05 01:10:20 +0900

    feat: 11~12강 - QuestionRepository에 findBySubject 메서드 구현, refactor: QuestionRepository를 테스트 클래스로 변경

--- src/test/java/com/back/QuestionRepositoryTest.java
+++ src/test/java/com/back/QuestionRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -43,15 +44,8 @@ public class QuestionRepositoryTest {
     @Transactional
     void t2() {
         Question q1 = questionRepository.findById(1).get();
-        System.out.println(q1.getSubject());
-        System.out.println(q1.getContent());
 
-        // q1 질문에 대한 답글
-        List<Answer> answers = q1.getAnswerList();
-
-        for (Answer a : answers) {
-            System.out.println(a.getContent());
-        }
+        assertThat(q1.getSubject()).isEqualTo("sbb가 무엇인가요?");
 
     }
 
@@ -60,7 +54,20 @@ public class QuestionRepositoryTest {
         List<Question> all = this.questionRepository.findAll();
         assertEquals(2, all.size());
 
+        System.out.println(all.size());
+
         Question q = all.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+        System.out.println(q.getSubject());
+    }
+
+    @Test
+    void t3(){
+        Question q1 = questionRepository.findBySubject("sbb가 무엇인가요?").get();
+
+        assertThat(q1.getId()).isEqualTo(1);
+        assertThat(q1.getContent()).isEqualTo("sbb에 대해서 알고 싶습니다.");
+
     }
 }
